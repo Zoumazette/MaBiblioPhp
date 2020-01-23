6ea8283c364cab22df2c73e054a69f065c730a15
Jean-michel Hinicker <36866750+JimHin@users.noreply.github.com>
2020-01-23 19:39:01 +0100

    Add files via upload

--- bibliophp.php
+++ bibliophp.php
@@ -1,18 +1,27 @@
 <?php
 
-/* TRAITEMENT DES CHAÎNES DE CARACTERE */
+/* TRAITEMENT DES CHAÎNES DE CARACTERES */
 
+
+# on supprime : majuscules ; / ? : @ & = + $ , . ! ~ * ( ) les espaces multiples et les underscore
 function nettoyerChaine($string)
 {
-    // on supprime : majuscules ; / ? : @ & = + $ , . ! ~ * ( ) les espaces multiples et les underscore
     $string = strtolower($string);
     $string = preg_replace("/[^a-z0-9_'\s-]/", "", $string);
     $string = preg_replace("/[\s-]+/", " ", $string);
     $string = preg_replace("/[\s_]/", " ", $string);
     return $string;
 }
-
+# fonction remplace les espaces " " par des - et enlève les caractères spéciaux
 function nettoyer($string) {
-    $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
-    return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+    $string = str_replace(' ', '-', $string); // Remplace les espaces par des tirets.
+    return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // efface les caractères spéciaux.
+}
+
+# fonction remplace les espaces " " par des - 
+function slug($z){
+    $z = strtolower($z);
+    $z = preg_replace('/[^a-z0-9 -]+/', '', $z);
+    $z = str_replace(' ', '-', $z);
+    return trim($z, '-');
 }
